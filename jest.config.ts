3ef5640454d86874f9654d5ad6cb51bbb9480cf4
Esteban Escobar <esteban.e.gomez@gmail.com>
2025-03-06 23:42:43 -0500

    fixing jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,14 @@ import type { Config } from "jest";
 
 const config: Config = {
   verbose: true,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  preset: "ts-jest/presets/default-esm",
 };
 
 export default config;
